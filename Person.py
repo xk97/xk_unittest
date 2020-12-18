ad55b5e44e61c438dfdc42007ad1d38904f6c38e
xk97 <xhkong@mail.utexas.edu>
2020-12-18 14:23:52 -0600

    s

--- Person.py
+++ Person.py
@@ -23,3 +23,5 @@ if __name__ == '__main__':
     person = Person()
     print('User Abbas has been added with id ', person.set_name('Abbas'))
     print('User associated with id 0 is ', person.get_name(0))
+    person2 = Person()
+    print(person2.get_name(0))
